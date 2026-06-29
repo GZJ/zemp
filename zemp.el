194a8e7bdf0ad7bdc0d88490d6604811cd95a163
gzj <gzj00@outlook.com>
2026-06-29 21:16:02 +0800

    chore: bump emacs version to 30.2

--- zemp.el
+++ zemp.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/GZJ/zemp
 ;; Keywords: music player
 ;; Version: 1.1.0
-;; Package-Requires: ((emacs "28.1"))
+;; Package-Requires: ((emacs "30.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
